cbc490d15e5a9321699e920c73538e64b989d032
ppp38 <cmullenx@gmail.com>
2016-10-27 16:35:13 -0500

    create component for left user tab

--- client/src/components/CurrentUsers.js
+++ client/src/components/CurrentUsers.js
@@ -3,8 +3,8 @@ import React from 'react'
 const CurrentUsers = (props) =>{
 
 	return(
-		<div class="userwrap">
-			<div class="currentuser">
+		<div className="curwrap">
+			<div className="onlineuser">
 			  <div className="curuseravi">Users photo</div>
         <div className="curusername">Users Name</div>
 			</div>
--- client/src/components/lobby.js
+++ client/src/components/lobby.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import OpenStory from './OpenStory'
-import Accordion from './Accordion'
 import LeaderBoard from './Leaderboard'
-import CurrentUsers from './CurrentUsers'
+import UserPanel from './UserPanel'
 
 
 class Lobby extends React.Component {
@@ -10,32 +8,19 @@ class Lobby extends React.Component {
   constructor (props) {
     super(props); 
     this.state = {
-      allStories: [],
-      openStories: [],
-      completeStories: [],
       users: [], 
-      displayComplete: false,
       displayLeader: false
     }
   this.toggleLeader = this.toggleLeader.bind(this);
-  this.toggleDisplay = this.toggleDisplay.bind(this)
   }
 
   componentDidMount () {
-    //get an array of all the stories from the db that need more users
-    $.get('/stories')
-    .then(stories => {
-      console.log('Got stories: ', stories);
-      let completeStories = stories.filter(story => story.complete)
-      console.log('comstor: ',completeStories)
-      let openStories = stories.filter(story => story.length > story.lines.length)
-      console.log('openstor: ', openStories)
-      this.setState({
-        allStories: stories,
-        openStories: openStories,
-        completeStories: completeStories
+   $.get('/user')
+      .then(user => {
+        console.log('this is the returned get user', user)
+        this.name = user.name
+        this.avi = user.profileImage
       })
-    })   
 
     $.get('/score')
     .then(users => {
@@ -46,12 +31,6 @@ class Lobby extends React.Component {
     })
   }
 
-  toggleDisplay () {
-    this.setState({
-      displayComplete: !this.state.displayComplete
-    })
-  }
-
   toggleLeader(){
     this.setState({
       displayLeader: !this.state.displayLeader
@@ -59,42 +38,25 @@ class Lobby extends React.Component {
   }
 
   render () {
-    var displayButtonText = this.state.displayComplete ? 'Show Open' : 'Show Complete'
     var displayLeaderText = this.state.displayLeader ? 'Close Leaderboard' : 'Show Leaderboard' 
     return (
       <div>
-        <Accordion />
         <div className='lobby'>
           <div className="lobbyLabels">
+            <div id="users">
+            <UserPanel name={this.name} avi={this.avi}/>
+            </div>
             <h4 className="storyNames">Story Name</h4>
             <h4 className="numberOfUsers">Number of users</h4>
-            <h4 className="toggleDisplayLink"><a onClick={this.toggleDisplay}>{displayButtonText}</a></h4>
             <h4 className="leaderbutton"><a onClick={this.toggleLeader}>{displayLeaderText}</a></h4>
           </div>
-          { this.state.displayComplete ?
-
-            this.state.completeStories.map((story, i) =>
-              <OpenStory story={story} key={i} />
-            )
-
-            :
-
-            this.state.openStories.map((story, i) =>
-              <OpenStory story={story} key={i} />
-            )
-            
-          }
           {
             this.state.displayLeader ? 
             this.state.users.map((user, i) =>
               <LeaderBoard user={user} key={i}/>
             )
-          
             : null 
           }
-        </div>
-        <div id="users">
-        <CurrentUsers />
         </div>
       </div>
     )

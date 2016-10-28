af404eccd1395774b6f5d09b1b81793d8db8d460
ppp38 <cmullenx@gmail.com>
2016-10-28 17:08:25 -0500

    change lobby ui

--- client/src/components/UserPanel.js
+++ client/src/components/UserPanel.js
@@ -6,8 +6,8 @@ const UserPanel= (props) =>{
 	return(
 		<div className="userwrap">
 			<div className="currentuser">
-		    <div className="currentAvi"><img src={props.avi}></img></div>
-	      <div className="currentName"><p className="name">{props.name}</p></div>
+		    <div className="currentavi"><img src={props.avi}></img></div>
+	      <div className="currentname"><p className="name">{props.name}</p></div>
 			</div>
 			<div className="loggedin">
 			<CurrentUsers />

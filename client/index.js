e9188423c6bff69a8d5478e0d21ac92d52cadfec
Christina Mullen <cmullenx@gmail.com>
2016-10-29 20:30:11 -0500

    Update index.js

--- client/index.js
+++ client/index.js
@@ -2,13 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './src/components/app'
 require('./public/styles/main.css')
-require('./public/styles/line.css')
-require('./public/styles/story.css')
 require('./public/styles/lobby.css')
-require('./public/styles/openStory.css')
 require('./public/styles/signup.css')
-require('./public/styles/createStory.css')
-require('./public/styles/accordion.css')
 require('./public/styles/navbar.css')
 
 ReactDOM.render(

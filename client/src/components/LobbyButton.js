66f14fbcaaac0d946d2212fdbe6e3c2b6c4b5014
Christina Mullen <cmullenx@gmail.com>
2016-10-29 19:51:56 -0500

    Create LobbyButton.js

---
+++ client/src/components/LobbyButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LobbyButton = (props) => {
+  return (
+    <a href="/lobby" className="standardButton blackButton">
+      Logout
+    </a>
+  )
+}
+
+export default LobbyButton
